5094e83c7f70728eec7e877e5689dd16bc818d4a
Mark Riedesel <mark@klowner.com>
2013-06-22 01:33:33 -0500

    added multi-vertex support

--- mesh_planarizer_0_1_1.py
+++ mesh_planarizer_0_1_1.py
@@ -21,7 +21,7 @@
 bl_info = {
     'name': 'Planarizer',
     'author': "Mark Riedesel",
-    'version': (0, 1, 0),
+    'version': (0, 1, 1),
     'blender': (2, 66, 3),
     'location': "Editmode > D",
     'warning': "",
@@ -41,10 +41,35 @@ def project_to_plane(point, va, vb):
     norm = va.cross(vb)
     norm.normalize()
 
-    adjust = mathutils.Vector()
-
     adjust = norm.dot(point)
     return norm.xyz * adjust
+
+
+def project_to_plane_normal(point, normal):
+    normal.normalize()
+    adjust = normal.dot(point)
+    return normal.xyz * adjust
+
+
+def get_face_closest_to_mouse(faces, context, mouse_pos):
+    ob = context.active_object
+    region = context.region
+    region_3d = context.space_data.region_3d
+
+    optimal_faces = []
+    min_dist = False
+
+    for face in faces:
+        face_pos = face.calc_center_median()
+        world_face_pos = ob.matrix_world * face_pos
+        screen_face_pos = view3d_utils.location_3d_to_region_2d(region,
+                                                                region_3d,
+                                                                world_face_pos)
+        dist = (mouse_pos - screen_face_pos).length
+        if not min_dist or dist < min_dist[0]:
+            min_dist = (dist, face)
+
+    return min_dist[1]
 
 
 def fix_nonplanar_face(bm, vert_sel, context, event):
@@ -58,26 +83,9 @@ def fix_nonplanar_face(bm, vert_sel, context, event):
     # Get all connected quads
     faces = [face for face in vert_sel.link_faces if len(face.verts) == 4]
 
-    # Find optimal face to project the selected vertex to
-    mouse_pos = mathutils.Vector([event.mouse_region_x, event.mouse_region_y])
-    optimal_faces = []
-    min_dist = False
-
-    for face in faces:
-        face_pos = face.calc_center_bounds()
-        world_face_pos = ob.matrix_world * face_pos
-        screen_face_pos = view3d_utils.location_3d_to_region_2d(region,
-                                                                region_3d,
-                                                                world_face_pos)
-
-        dist = (mouse_pos - screen_face_pos).length
-        if not min_dist or dist < min_dist[0]:
-            min_dist = (dist, face)
-
-    optimal_faces.append(min_dist)
-
     # Find edges that do not contain selected vertex
-    face = optimal_faces[0][1]
+    mouse_pos = mathutils.Vector([event.mouse_region_x, event.mouse_region_y])
+    face = get_face_closest_to_mouse(faces, context, mouse_pos)
 
     # Find the unselected vertices of the face
     face_verts = [v for v in face.verts if not v.select]
@@ -106,15 +114,49 @@ def fix_nonplanar_face(bm, vert_sel, context, event):
 
     vert_sel.co.xyz = (vert_sel.co - new_vertex)
 
-    # Update display
-    bpy.ops.object.mode_set(mode='OBJECT')
-    bpy.ops.object.mode_set(mode='EDIT')
+
+def fix_multi_nonplanar_verts(bm, vert_sel, context, event):
+    ob = context.active_object
+    region = context.region
+    region_3d = context.space_data.region_3d
+
+    # Find face under mouse cursor
+    mouse_pos = mathutils.Vector([event.mouse_region_x, event.mouse_region_y])
+    face = get_face_closest_to_mouse(bm.faces, context, mouse_pos)
+
+    # Edges connected to selected vertices
+
+    # Find an unselected vertex shared by a selected vertices' edg
+    ref_vert = None
+    for v in vert_sel:
+        for edge in v.link_edges:
+            for edge_v in edge.verts:
+                if edge_v not in vert_sel:
+                    ref_vert = edge_v
+
+        if ref_vert:
+            ref_vert = ref_vert.co
+            break
+
+    # All connected verts must be selected, so we fall-back
+    # to using the median point of the selected face
+    if not ref_vert:
+        ref_vert = face.calc_center_median()
+
+    normal = face.normal
+
+    # Project selected points to nearest point on plane defined
+    # by the hovered face's surface normal
+    for v in vert_sel:
+        local_v = v.co - ref_vert
+        adjustment = project_to_plane_normal(local_v, normal)
+        v.co.xyz = (v.co.xyz - adjustment)
 
 
 class MeshPlanarizer(bpy.types.Operator):
     """Tooltip"""
     bl_idname = "mesh.planarizer"
-    bl_label = "Fix non-planar quad"
+    bl_label = "Fix non-planar face"
     bl_options = {'REGISTER', 'UNDO'}
 
     @classmethod
@@ -126,7 +168,16 @@ class MeshPlanarizer(bpy.types.Operator):
         bm = bmesh.from_edit_mesh(context.active_object.data)
         selected_verts = [v for v in bm.verts if v.select]
 
-        fix_nonplanar_face(bm, selected_verts[0], context, event)
+        if not selected_verts:
+            return {'CANCELLED': "Error"}
+
+        if len(selected_verts) > 1:
+            fix_multi_nonplanar_verts(bm, selected_verts, context, event)
+        else:
+            fix_nonplanar_face(bm, selected_verts[0], context, event)
+
+        bmesh.update_edit_mesh(context.active_object.data)
+
         return {'FINISHED'}
 
 
